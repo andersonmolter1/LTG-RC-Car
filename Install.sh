bf2f9ce49c88e1a997635270a585875f6de0e795
Anderson Molter <andersonmolter1@gmail.com>
2020-03-14 16:03:37 -0400

    Added integral step and created array of previous error values

--- Install.sh
+++ Install.sh
@@ -3,10 +3,13 @@ HN = $1
 sudo apt-get update
 sudo apt-get dist-upgrade
 sudo apt-get install python-rpi.gpio python3-rpi.gpio
+pip install sympy
+y | sudo apt-get install emacs
 sudo service dhcpcd start
 sudo systemctl enable dhcpcd
 git clone https://github.com/silvanmelchior/RPi_Cam_Web_Interface.git
 cd RPi_Cam_Web_Interface
-./install.sh
+y | ./install.sh
+pip install sympy
 sudo hostname hN
 sudo reboot
