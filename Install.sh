130b9da1349f6fa011b6fa13fb2bb4d114a9c9ef
unknown <andersonmolter1@gmail.com>
2020-03-18 00:03:53 -0400

    Updated Install Script

--- Install.sh
+++ Install.sh
@@ -4,9 +4,7 @@ yes | sudo apt-get dist-upgrade
 yes | sudo apt-get install python-rpi.gpio python3-rpi.gpio
 yes | sudo apt-get install python3-pip
 pip install sympy
-echo "cd LTG-RC-Car" >> .profile
-echo "git pull" >> .profile
-echo "cd .." >> .profile
+echo "find . -name \".git\" -type d | sed 's/\/.git//' |  xargs -P10 -I{} git -C {} pull" >> .profile
 yes | sudo apt-get install emacs
 git clone https://github.com/silvanmelchior/RPi_Cam_Web_Interface.git
 cd RPi_Cam_Web_Interface
