761d8f7915896c3743da4697727add7d70970933
unknown <andersonmolter1@gmail.com>
2020-03-17 21:11:51 -0400

    Modified Install script

--- Install.sh
+++ Install.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-HN = $1 
 sudo apt-get update
 yes | sudo apt-get dist-upgrade
-sudo apt-get install python-rpi.gpio python3-rpi.gpio
-sudo apt-get install python3-pip
-pip install sympy
-y | sudo apt-get install emacs
+yes | sudo apt-get install python-rpi.gpio python3-rpi.gpio
+yes | sudo apt-get install python3-pip
+yes | pip install sympy
+yes | sudo apt-get install emacs
 git clone https://github.com/silvanmelchior/RPi_Cam_Web_Interface.git
 cd RPi_Cam_Web_Interface
-y | ./install.sh
-pip install sympy
-sudo hostname hN
+yes | ./install.sh
+sudo hostname $1
 sudo reboot
