0823596ff2b91235b183f1b547fd1ade56a5455a
Anderson Molter <ajm70549@uga.edu>
2020-07-16 19:45:24 -0400

    Changed server script.

--- UnityCommuncation/Server.py
+++ UnityCommuncation/Server.py
@@ -1,13 +1,54 @@
-import socket #import the socket module
+# import socket #import the socket module
 
-s = socket.socket() #Create a socket object
-host = socket.gethostname() #Get the local machine
-port = 12397 # Reserve a port for your service
-s.bind(('', port)) #Bind to the port
-s.listen(5) #Wait for the client connection
-error = '1'
-while True:
-    c,addr = s.accept() #Establish a connection with the client
-    print "Got connection from", addr
-    c.send(error)
-    c.close()
+# class MySocket:
+#     def __init__(self):
+    
+#     def connect(self, host, port):
+#         s = socket.socket() #Create a socket object
+#         host = socket.gethostname() #Get the local machine
+#         port = 5000 # Reserve a port for your service
+#         s.bind(('', port)) #Bind to the port
+#         s.listen(5) #Wait for the client connection
+#         error = '1'
+#         while True:
+#             c,addr = s.accept() #Establish a connection with the client
+#             print("Got connection from" + addr)
+#             c.send(error)
+#             c.recv
+#             c.close()
+import socket
+class MySocket:
+    """demonstration class only
+      - coded for clarity, not efficiency
+    """
+
+    def __init__(self, sock=None):
+        if sock is None:
+            self.sock = socket.socket(
+                            socket.AF_INET, socket.SOCK_STREAM)
+        else:
+            self.sock = sock
+
+    def connect(self, host, port):
+        self.sock.connect((host, port))
+
+    def mysend(self, msg):
+        totalsent = 0
+        while totalsent < MSGLEN:
+            sent = self.sock.send(msg[totalsent:])
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            totalsent = totalsent + sent
+
+    def myreceive(self):
+        chunks = []
+        bytes_recd = 0
+        while bytes_recd < MSGLEN:
+            chunk = self.sock.recv(min(MSGLEN - bytes_recd, 2048))
+            if chunk == b'':
+                raise RuntimeError("socket connection broken")
+            chunks.append(chunk)
+            bytes_recd = bytes_recd + len(chunk)
+        return b''.join(chunks)
+s = MySocket()
+s.connect()
