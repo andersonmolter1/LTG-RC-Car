75296f43f0f73224219a27b3373da303261e4ba3
Anderson Molter <ajm70549@uga.edu>
2020-09-02 14:25:16 -0400

    Redid Motor Control for clarity and consistency

---
+++ MotorControl.py
@@ -0,0 +1,72 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+
+
+
+class MotorControl:
+	PWMValue=""
+	mf_pwm=""
+	mb_pwm=""
+	mf_p1=""
+	mf_p2=""
+
+	mb_p2=""
+	mb_p1=""
+	pwmSpeed=""
+	def __init__(self):
+		self.pwmSpeed=80
+		self.pwmSpeed1=80
+		self.pwmSpeed2=80
+		self.mb_p1=11
+		self.mb_p2=12
+		self.mf_p1=15
+		self.mf_p2=16
+		self.PWMValue = 100
+		GPIO.setmode(GPIO.BOARD)
+		GPIO.setup(7,GPIO.OUT)
+		GPIO.setup(13,GPIO.OUT)
+		self.mb_pwm=GPIO.PWM(7,self.PWMValue)
+		self.mf_pwm=GPIO.PWM(13,self.PWMValue)
+		GPIO.setup(self.mf_p1,GPIO.OUT,initial=0)
+		GPIO.setup(self.mf_p2,GPIO.OUT,initial=0) 
+		GPIO.setup(self.mb_p1,GPIO.OUT,initial=0) 
+		GPIO.setup(self.mb_p2,GPIO.OUT,initial=0) 
+		self.mf_pwm.start(0)
+		self.mb_pwm.start(0)
+	
+	def MoveForward(self):
+		GPIO.output(self.mb_p1,GPIO.HIGH)
+		GPIO.output(self.mb_p2,GPIO.LOW)
+		GPIO.output(self.mf_p1,GPIO.LOW)
+		GPIO.output(self.mf_p2,GPIO.LOW)
+		self.mb_pwm.ChangeDutyCycle(self.pwmSpeed)
+	def MoveBackward(self):
+		GPIO.output(self.mb_p1,GPIO.LOW)
+		GPIO.output(self.mb_p2,GPIO.HIGH)
+		GPIO.output(self.mf_p1,GPIO.LOW)
+		GPIO.output(self.mf_p1,GPIO.LOW)
+		self.mb_pwm.ChangeDutyCycle(self.pwmSpeed)
+	def MoveLeft(self):
+		GPIO.output(self.mb_p1,GPIO.HIGH)
+		GPIO.output(self.mb_p2,GPIO.LOW)
+		GPIO.output(self.mf_p1,GPIO.HIGH)
+		GPIO.output(self.mf_p2,GPIO.LOW)
+		self.mb_pwm.ChangeDutyCycle(self.pwmSpeed2)
+		self.mf_pwm.ChangeDutyCycle(self.pwmSpeed1)
+	def MoveRight(self):
+		GPIO.output(self.mb_p1,GPIO.HIGH)
+		GPIO.output(self.mb_p2,GPIO.LOW)
+		GPIO.output(self.mf_p1,GPIO.LOW)
+		GPIO.output(self.mf_p2,GPIO.HIGH)
+		self.mb_pwm.ChangeDutyCycle(self.pwmSpeed2)
+		self.mf_pwm.ChangeDutyCycle(self.pwmSpeed1)
+	def Stopper(self):
+		GPIO.output(self.mb_p1,GPIO.LOW)
+		GPIO.output(self.mb_p2,GPIO.LOW)
+		GPIO.output(self.mf_p1,GPIO.LOW)
+		GPIO.output(self.mf_p2,GPIO.LOW)
+		
+		self.mf_pwm.ChangeDutyCycle(0)
+
+
---
+++ PIDController.py
@@ -0,0 +1,85 @@
+from time import sleep
+import sys
+from datetime import datetime
+from motorControl import MotorControl
+
+class PIDController:
+    controller = MotorControl()
+    J_P = 25  # Proportion value
+    J_I = 0  # Integral Step value
+    J_D = 0  # Derivative Step Value
+    error = 0  # amount of error on the line the car is experiencing
+    PV = 0  # list of all values errors that the car has experienced
+    prevError = 0 # error of last calculation used for Derivative calc
+
+    def Speed(self):  # Gets speed proportional to error term
+        return (50 - (abs(self.error) * 8))
+
+    def Proportion(self):  # Calculates P of PID multiplied by the its constant
+        return (self.error * self.J_P)
+
+    def Integral(self):  # Calculates I of PID multiplied by the its constant
+        return (self.PV * self.J_I) 
+
+    def Derivative(self):  # Caluclates D of PID multiplied by the its constant
+        return ((self.error - self.prevError) * self.J_D) 
+
+    def PID(self):  # Returns PID model
+        return abs(self.Proportion() - self.Derivative() - self.Integral())
+
+    def driveCar(self):
+        line = 1  # if no argument given, will default to line being black with a white background
+        noLine = 0
+        if (len(sys.argv) > 1 and sys.argv[1] == 2):
+            line = 0
+            noLine = 1
+        while True:
+            sleep(0.0075)
+            RR = GPIO.input(29)  # Right Right Sensor
+            RM = GPIO.input(31)  # Right Middle Sensor
+            MM = GPIO.input(33)  # Middle Middle Sensor
+            LM = GPIO.input(35)  # Left Middle Sensor
+            LL = GPIO.input(37)  # Left Left Sensor
+            print(f'{LL:d} {LM:d} {MM:d} {RM:1d} {RR:d}')
+            # 0 0 0 0 1 ==> Error = 4
+            # 0 0 0 1 1 ==> Error = 3
+            # 0 0 0 1 0 ==> Error = 2
+            # 0 0 1 1 0 ==> Error = 1
+            # 0 0 1 0 0 ==> Error = 0
+            # 0 1 1 0 0 ==> Error = -1
+            # 0 1 0 0 0 ==> Error = -2
+            # 1 1 0 0 0 ==> Error = -3
+            # 1 0 0 0 0 ==> Error = -4
+            if (LL == noLine and LM == noLine and MM == noLine and RM == noLine and RR == noLine):
+                controller.Stopper()
+            elif (LL == noLine and LM == noLine and MM == noLine and RM == noLine and RR == line):
+                self.error = 4
+                controller.MoveRight(1)
+            elif (LL == noLine and LM == noLine and MM == noLine and RM == line and RR == line):
+                self.error = 3
+                controller.MoveRight(.75)
+            elif (LL == noLine and LM == noLine and MM == noLine and RM == line and RR == noLine):
+                self.error = 2
+                controller.MoveRight(.50)
+            elif (LL == noLine and LM == noLine and MM == line and RM == line and RR == noLine):
+                self.error = 1
+                controller.MoveRight(.25)
+            elif (LL == noLine and LM == noLine and MM == line and RM == noLine and RR == noLine):
+                controller.noError()
+            elif (LL == noLine and LM == line and MM == line and RM == noLine and RR == noLine):
+                self.error = -1
+                controller.MoveLeft(.25)
+            elif (LL == noLine and LM == line and MM == noLine and RM == noLine and RR == noLine):
+                self.error = -2
+                controller.MoveLeft(.50)
+            elif (LL == line and LM == line and MM == noLine and RM == noLine and RR == noLine):
+                self.error = -3
+                controller.MoveLeft(.75)
+            elif (LL == line and LM == noLine and MM == noLine and RM == noLine and RR == noLine):
+                self.error = -4
+                controller.MoveLeft(1)
+            else:
+                dump = 0
+car = PIDController()
+car.initialize()
+car.driveCar()
