0ffd1d17ea2c8ab09028ae0eb8e2ef9936b2e495
Anderson Molter <andersonmolter1@gmail.com>
2020-11-06 18:37:08 -0500

    Update Install.sh

--- Setup/Install.sh
+++ Setup/Install.sh
@@ -4,4 +4,5 @@ yes | sudo apt-get dist-upgrade
 yes | sudo apt-get install python-rpi.gpio python3-rpi.gpio
 yes | sudo apt-get install python3-pip
 sudo pip install sparkfun-qwiic-scmd
+sudo apt install git-all
 echo "find . -name \".git\" -type d | sed 's/\/.git//' |  xargs -P10 -I{} git -C {} pull" >> .profile
