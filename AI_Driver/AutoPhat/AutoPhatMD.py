86ca6936920e20a4e8994602a8ac2e685801a364
Anderson Molter <andersonmolter1@gmail.com>
2021-01-22 02:50:13 +0000

    Changes to motor driver

--- AI_Driver/AutoPhat/AutoPhatMD.py
+++ AI_Driver/AutoPhat/AutoPhatMD.py
@@ -13,29 +13,29 @@ class AutoPhatMD:
         if (error > self.pastError):
             for i in range (self.pastError, error, 25):
                 self.myMotor.set_drive(0, 1, i)
-                self.myMotor.set_drive(1, 0, 255 - i)
+                self.myMotor.set_drive(1, 0, 200 - i)
             self.pastError = error
             return
         self.myMotor.set_drive(0, 1, error)
-        self.myMotor.set_drive(1, 0, 255 - error)
+        self.myMotor.set_drive(1, 0, 200 - error)
         self.pastError = error
     def TurnRight(self, error):
         if (error > self.pastError):
             for i in range (self.pastError, error, 25):
-                self.myMotor.set_drive(0, 1, i)
-                self.myMotor.set_drive(1, 0, 255 - i)
+                self.myMotor.set_drive(0, 0, i)
+                self.myMotor.set_drive(1, 0, 200 - i)
             self.pastError = error
             return
-        self.myMotor.set_drive(0, 1, error)
-        self.myMotor.set_drive(1, 0, 255 - error)
+        self.myMotor.set_drive(0, 0, error)
+        self.myMotor.set_drive(1, 0, 200 - error)
         self.pastError = error
     def Stop(self):
         self.myMotor.set_drive(0, 0, 0)
-        self.myMotor.set_drive(1, 0, 0)
+        #self.myMotor.set_drive(1, 0, 0)
     def NoError(self):
         self.myMotor.set_drive(0, 0, 0)
-        for i in range (100, 200, 25):
-                self.myMotor.set_drive(1, 0, i)
+        #for i in range (100, 200, 25):
+                #self.myMotor.set_drive(1, 0, i)
     def __init__(self):
         R_MTR = 0
         L_MTR = 1
