ef0ecf8869a7ead12cd7d0d86f7d46661a2f30d6
Anderson Molter <andersonmolter1@gmail.com>
2021-04-22 18:12:14 -0400

    Update Install.sh

--- Setup/Install.sh
+++ Setup/Install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-sudo apt-get update
-yes | sudo apt-get dist-upgrade
-yes | sudo apt-get install python-rpi.gpio python3-rpi.gpio -y
-yes | sudo apt-get install python3-pip -y
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install python3-pip -y
+sudo apt-get install python3-rpi.gpio python3-rpi.gpio -y
 sudo pip install sparkfun-qwiic-scmd -y
-sudo apt install git-all -y
+echo "Enter static IP:"
+read static_ip
+echo "interface wlan0\nstatic ip_address=${static_ip}/24\nstatic routers=192.168.1.1\nstatic domain_name_servers=8.8.8.8 4.4.4.4\nstatic domain_search=\nnoipv6" >> /etc/dhcpcd.conf
 echo "find . -name \".git\" -type d | sed 's/\/.git//' |  xargs -P10 -I{} git -C {} pull" >> .profile
