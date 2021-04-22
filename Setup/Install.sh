08d371bbe7694048a8afb7fc7628fed59bc22c23
Anderson Molter <andersonmolter1@gmail.com>
2021-04-22 19:25:49 -0400

    Update Install.sh

--- Setup/Install.sh
+++ Setup/Install.sh
@@ -5,5 +5,11 @@ sudo apt-get install python3-rpi.gpio python3-rpi.gpio -y
 sudo pip install sparkfun-qwiic-scmd -y
 echo "Enter static IP:"
 read static_ip
-echo "interface wlan0\nstatic ip_address=${static_ip}/24\nstatic routers=192.168.1.1\nstatic domain_name_servers=8.8.8.8 4.4.4.4\nstatic domain_search=\nnoipv6" >> /etc/dhcpcd.conf
+echo "Enter routers IP:"
+read router_ip
+echo "interface wlan0\nstatic ip_address=${static_ip}/24" >> /etc/dhcpcd.conf
+echo "static routers=${router_ip}" >> /etc/dhcpcd.conf
+echo "static domain_name_servers=8.8.8.8 4.4.4.4" >> /etc/dhcpcd.conf 
+echo "static domain_search=" >> /etc/dhcpcd.conf
+echo "noipv6" >> /etc/dhcpcd.conf
 echo "find . -name \".git\" -type d | sed 's/\/.git//' |  xargs -P10 -I{} git -C {} pull" >> .profile
