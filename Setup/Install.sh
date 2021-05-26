7da9ba2a192b18bb078b6ccf1d4f5a4145ac56eb
Anderson Molter <andersonmolter1@gmail.com>
2021-05-26 01:21:07 -0400

    Update Install.sh

--- Setup/Install.sh
+++ Setup/Install.sh
@@ -14,8 +14,6 @@ echo "interface wlan0" >> /etc/dhcpcd.conf
 echo "static ip_address=${static_ip}/24" >> /etc/dhcpcd.conf
 echo "static routers=${router_ip}" >> /etc/dhcpcd.conf
 echo "static domain_name_servers=8.8.8.8 4.4.4.4" >> /etc/dhcpcd.conf 
-echo "static domain_search=" >> /etc/dhcpcd.conf
-echo "noipv6" >> /etc/dhcpcd.conf
 echo "find . -name \".git\" -type d | sed 's/\/.git//' |  xargs -P10 -I{} git -C {} pull" >> .profile
 git clone https://github.com/silvanmelchior/RPi_Cam_Web_Interface.git
 cd RPi_Cam_Web_Interface
